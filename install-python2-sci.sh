2150ca59abd6e0b01dc759050369fe7a0d2f64c4
Grey Christoforo <grey@christoforo.net>
2017-03-14 17:45:30 +0000

    Update install-python2-sci.sh

--- install-python2-sci.sh
+++ install-python2-sci.sh
@@ -13,6 +13,7 @@ pacman -S --noconfirm --noprogress --needed python2-mpi4py
 
 # and some things from the AUR
 su docker -c 'pacaur -m --noprogressbar --noedit --noconfirm python2-rpyc-fix'
+su docker -c 'pacaur -m --noprogressbar --noedit --noconfirm python2-objgraph'
 su docker -c 'pacaur -m --noprogressbar --noedit --noconfirm python2-dill-fix'
 su docker -c 'pacaur -m --noprogressbar --noedit --noconfirm python2-pfft-fix'
 su docker -c 'pacaur -m --noprogressbar --noedit --noconfirm python2-pox'
