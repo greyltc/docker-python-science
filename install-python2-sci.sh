d846c2fb0114bdcd0b2c45ff0de236d9ec2e6261
Grey Christoforo <grey@christoforo.net>
2017-03-23 09:52:00 +0000

    add --needed to installs from aur

--- install-python2-sci.sh
+++ install-python2-sci.sh
@@ -12,17 +12,17 @@ pacman -S --noconfirm --noprogress --needed python2-sqlalchemy
 pacman -S --noconfirm --noprogress --needed python2-mpi4py
 
 # and some things from the AUR
-su docker -c 'pacaur -S --noprogressbar --noedit --noconfirm python2-rpyc-fix'
-su docker -c 'pacaur -S --noprogressbar --noedit --noconfirm python2-objgraph'
-su docker -c 'pacaur -S --noprogressbar --noedit --noconfirm python2-dill-fix'
-su docker -c 'pacaur -S --noprogressbar --noedit --noconfirm python2-ppft-fix'
-su docker -c 'pacaur -S --noprogressbar --noedit --noconfirm python2-multiprocess-fix'
-su docker -c 'pacaur -S --noprogressbar --noedit --noconfirm python2-pox'
-su docker -c 'pacaur -S --noprogressbar --noedit --noconfirm python2-pathos-3fix'
-su docker -c 'pacaur -S --noprogressbar --noedit --noconfirm python2-pyina-git'
-su docker -c 'pacaur -S --noprogressbar --noedit --noconfirm python2-joblib'
-su docker -c 'pacaur -S --noprogressbar --noedit --noconfirm python2-klepto'
-su docker -c 'pacaur -S --noprogressbar --noedit --noconfirm python2-mystic'
+su docker -c 'pacaur -S --noprogressbar --needed --noedit --noconfirm python2-rpyc-fix'
+su docker -c 'pacaur -S --noprogressbar --needed --noedit --noconfirm python2-objgraph'
+su docker -c 'pacaur -S --noprogressbar --needed --noedit --noconfirm python2-dill-fix'
+su docker -c 'pacaur -S --noprogressbar --needed --noedit --noconfirm python2-ppft-fix'
+su docker -c 'pacaur -S --noprogressbar --needed --noedit --noconfirm python2-multiprocess-fix'
+su docker -c 'pacaur -S --noprogressbar --needed --noedit --noconfirm python2-pox'
+su docker -c 'pacaur -S --noprogressbar --needed --noedit --noconfirm python2-pathos-3fix'
+su docker -c 'pacaur -S --noprogressbar --needed --noedit --noconfirm python2-pyina-git'
+su docker -c 'pacaur -S --noprogressbar --needed --noedit --noconfirm python2-joblib'
+su docker -c 'pacaur -S --noprogressbar --needed --noedit --noconfirm python2-klepto'
+su docker -c 'pacaur -S --noprogressbar --needed --noedit --noconfirm python2-mystic'
 
 # reduce docker layer size
 cleanup-image
